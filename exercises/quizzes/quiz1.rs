c754747eff3116a4068cf1acdaa77b8005b0969e
Hieu Nguyen <nhthieu16@proton.me>
2025-05-28 15:32:36 +0700

    4,5,6

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,7 +10,13 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(number: u32) -> u32 {
+    if number > 40 {
+        number
+    } else {
+        number * 2
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
