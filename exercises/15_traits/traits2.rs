e256cbcabb266fdf8049754c02928ed0342b32f5
nibtr <nhthieu16@proton.me>
2025-06-03 19:13:06 +0700

    14,15,16,17

--- exercises/15_traits/traits2.rs
+++ exercises/15_traits/traits2.rs
@@ -4,6 +4,13 @@ trait AppendBar {
 
 // TODO: Implement the trait `AppendBar` for a vector of strings.
 // `append_bar` should push the string "Bar" into the vector.
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut v = self.to_vec();
+        v.push(String::from("Bar"));
+        v
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
